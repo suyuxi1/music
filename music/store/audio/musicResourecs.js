aa860b673a3bb248f508e37ebb8c769338d19a79
su <2296887348@qq.com>
2020-11-11 23:17:28 +0800

    项目初始化，添加音乐播放

---
+++ music/store/audio/musicResourecs.js
@@ -0,0 +1,48 @@
+export default {
+	musicResourecs: [{
+			id: 2001,
+			name: "爱得太迟",
+			src: "/static/mp3/爱得太迟.mp3",
+			singer: {
+				name: "古巨基",
+				synopsis: "11111111111111"
+			}
+		},
+		{
+			id: 2002,
+			name: "不再犹豫",
+			src: "/static/mp3/不再犹豫.mp3",
+			singer: {
+				name: "Beyond",
+				synopsis: "2222222222"
+			}
+		},
+		{
+			id: 2003,
+			name: "甘心替代你",
+			src: "/static/mp3/甘心替代你.mp3",
+			singer: {
+				name: "郑伊健",
+				synopsis: "33333333333"
+			}
+		},
+		{
+			id: 2004,
+			name: "你是人间四月天",
+			src: "/static/mp3/你是人间四月天.mp3",
+			singer: {
+				name: "邵帅",
+				synopsis: "444444444"
+			}
+		},
+		{
+			id: 2005,
+			name: "最佳损友",
+			src: "/static/mp3/最佳损友.mp3",
+			singer: {
+				name: "陈奕迅",
+				synopsis: "555555555"
+			}
+		},
+	]
+}
